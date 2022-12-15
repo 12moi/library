588106f51ce15376121fbeaa88b48833c761e785
devmoi <moishadrack.com>
2022-12-15 19:15:34 +0300

    add changes

--- script.js
+++ script.js
@@ -118,4 +118,43 @@ const resetBooksGrid = () => {
   booksGrid.innerHTML = ''
 }
 
+
+
+const createBookCard = (book) => {
+    const bookCard = document.createElement('div')
+    const title = document.createElement('p')
+    const author = document.createElement('p')
+    const pages = document.createElement('p')
+    const buttonGroup = document.createElement('div')
+    const readBtn = document.createElement('button')
+    const removeBtn = document.createElement('button')
+  
+    bookCard.classList.add('book-card')
+    buttonGroup.classList.add('button-group')
+    readBtn.classList.add('btn')
+    removeBtn.classList.add('btn')
+    readBtn.onclick = toggleRead
+    removeBtn.onclick = removeBook
+  
+    title.textContent = `"${book.title}"`
+    author.textContent = book.author
+    pages.textContent = `${book.pages} pages`
+    removeBtn.textContent = 'Remove'
+  
+    if (book.isRead) {
+      readBtn.textContent = 'Read'
+      readBtn.classList.add('btn-light-green')
+    } else {
+      readBtn.textContent = 'Not read'
+      readBtn.classList.add('btn-light-red')
+    }
+  
+    bookCard.appendChild(title)
+    bookCard.appendChild(author)
+    bookCard.appendChild(pages)
+    buttonGroup.appendChild(readBtn)
+    buttonGroup.appendChild(removeBtn)
+    bookCard.appendChild(buttonGroup)
+    booksGrid.appendChild(bookCard)
+  }
   
